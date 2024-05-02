128ab75c794de4971ce21b7c7c72917ed0820f10
HalimaSaleh <159858703+HalimaSaleh@users.noreply.github.com>
2024-05-01 21:05:28 -0400

    final

--- EMPLOYEE.sql
+++ EMPLOYEE.sql
@@ -1,19 +1,37 @@
-CREATE TABLE departments (
-    dept_no VARCHAR(5) PRIMARY KEY,
-    dept_name VARCHAR(255)
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/x6RlqX
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(5)   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
-select* from departments;
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(5)   NOT NULL
+);
 
-CREATE TABLE employees (
-    emp_no INTEGER PRIMARY KEY,
-    emp_title_id VARCHAR(5),
-    birth_date VARCHAR NOT NULL,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    sex CHAR(1),
-    hire_date VARCHAR NOT NULL
-	
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(5)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
+    "birth_date" VARCHAR  NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "sex" CHAR(1)   NOT NULL,
+    "hire_date" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 ALTER TABLE employees
 ALTER COLUMN birth_date TYPE DATE using to_date(birth_date , 'MM/DD/YYYY');
@@ -23,30 +41,34 @@ ALTER COLUMN hire_date TYPE DATE using to_date(hire_date , 'MM/DD/YYYY');
 
 SELECT * FROM employees;
 
-
-CREATE TABLE salaries (
-    emp_no INTEGER,
-    salary INTEGER,
-    PRIMARY KEY (emp_no, salary),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-select * from salaries;
-
-CREATE TABLE dept_emp (
-    emp_no INTEGER,
-    dept_no VARCHAR(5),
-    PRIMARY KEY (emp_no, dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL
 );
 
-select * from dept_emp;
-
-CREATE TABLE dept_manager (
-    dept_no VARCHAR(5),
-    emp_no INTEGER,
-    PRIMARY KEY (dept_no, emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
-select * from dept_manager;
+
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_manager" ("dept_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
+REFERENCES "employees" ("emp_title_id");
+
